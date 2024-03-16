0f2c125629783b491c811eab87a5b9fc3de7d73b
Manel-Bel <manelbelguenbour@gmail.com>
2024-03-16 16:44:59 +0100

    map

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ st.title("Motor vehicle Collisions in New York City")
 st.markdown("This application is a Steamlit dashboard that can be used to analyse motor vehicle collisions in NYC 🗽💥🚗")
 
 # rerun the function only if the code or input changed
-@st.cache_data(persist=True)
+@st.cache(persist=True)
 def load_data(nrows) :
     data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE','CRASH_TIME']])
 
@@ -31,6 +31,30 @@ def load_data(nrows) :
 
 data = load_data(100000)
 
+
+#filtering data
+
+st.header("Where are the most injured people in NYC ?")
+
+
+injured_poeple = st.slider("Number of Injured People in vehicle collisions", min_value=0, max_value=19)
+
+#plot the data into a map
+#filtering with query
+#column name >= variable , return the ln and lat columns
+st.map(data.query("injured_persons >= @injured_poeple")[["latitude","longitude"]].dropna(how="any")) 
+
+#update the view on the map
+
+st.header("How many collision occur during a given time of the day")
+
+# hour = st.selectbox("Hour to loor at",range(0,24), 1)  slider is better
+
+hour = st.slider("Hour to loor at",range(0,23)) 
+# hour = st.sidebar.slider("Hour to loor at",range(0,23)) 
+
+#subset the data so that the date/time column match  the hour selected
+data = data[data['date/time'].dt.hour == hour]
 
 
 
